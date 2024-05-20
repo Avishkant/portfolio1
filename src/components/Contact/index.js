778ddd1b1a3ef839d58d8d8471e8b41176a27512
Avishkant <avishkantpatidar22@gmail.com>
2024-05-20 19:57:31 +0530

    Updations

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -144,7 +144,7 @@ const Contact = () => {
   return (
     <Container>
       <Wrapper>
-        <Title>Get in Touch</Title>
+        <Title>Get in Touch Update</Title>
         <Desc>Feel free to reach out to me for any questions or opportunities!</Desc>
         <ContactForm ref={form} onSubmit={handleSubmit}>
           <ContactTitle>Email Me 🚀</ContactTitle>
